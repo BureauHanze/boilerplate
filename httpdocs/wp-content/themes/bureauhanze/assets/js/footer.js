35c425f20c77dca4054e051fdf2057eec3f7b002
Bart Nijenhuis <bartnijenhuis@bureauhanze.nl>
2021-12-30 20:57:01 +0100

    styling updates

--- httpdocs/wp-content/themes/bureauhanze/assets/js/footer.js
+++ httpdocs/wp-content/themes/bureauhanze/assets/js/footer.js
@@ -45,3 +45,19 @@ function stickyHeader() {
      header.classList.remove("--sticky");
    }
 }
+
+// Add active class on current link
+function highlightCurrent() {
+    const curPage = document.URL;
+    const links = document.getElementsByTagName('a');
+    for (let link of links) {
+        if (link.href == curPage) {
+        link.classList.add("--active");
+        }
+    }
+}
+document.onreadystatechange = () => {
+    if (document.readyState === 'complete') {
+        highlightCurrent()
+    }
+};
