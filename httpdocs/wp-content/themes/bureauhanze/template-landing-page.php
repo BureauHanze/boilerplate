f6405b52fc78e447234bef60be4382aa134ed161
Bart Nijenhuis <bartnijenhuis@bureauhanze.nl>
2022-05-09 10:08:24 +0200

    style change flex content

---
+++ httpdocs/wp-content/themes/bureauhanze/template-landing-page.php
@@ -0,0 +1,17 @@
+<?php 
+
+/* Template Name: template-landing-page */
+
+get_header(); ?>
+
+<main id="main" class="site-main" role="main">
+
+    <?php
+    get_template_part( 'template-parts/sections/flex-content' ); 
+
+    get_template_part( 'template-parts/socials/whatsapp-icon' ); 
+    ?>
+</main>
+
+<?php
+get_footer();
