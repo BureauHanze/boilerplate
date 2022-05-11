9a5a0823d61acd216a78056c031b2cfc02d19fcc
Bart Nijenhuis <bartnijenhuis@bureauhanze.nl>
2022-05-11 16:43:01 +0200

    added movie flex content

--- httpdocs/wp-content/themes/bureauhanze/template-parts/sections/flex-content.php
+++ httpdocs/wp-content/themes/bureauhanze/template-parts/sections/flex-content.php
@@ -21,21 +21,37 @@ if( have_rows('section_flex-content') ): ?>
                 endif; ?>
             </div>
         </div>
-        <?php
-
-    elseif( get_row_layout() == 'section_flex-content-text_row' ): ?>
-    <div class="row row-flex-content-text">
-        <?php
-        if(get_sub_field('section_flex-content_text-columns-heading')) :
-            the_sub_field('section_flex-content_text-columns-heading');
-        endif; ?>
-        <p class="flex-content__text" style="column-count: <?php the_sub_field('section_flex-content_text-columns-amount'); ?>">
-            <?php
-            if(get_sub_field('section_flex-content_text-columns')) :
-                the_sub_field('section_flex-content_text-columns', false, false);
+    <?php
+    elseif( get_row_layout() == 'section_flex-content-movie_row' ): ?>
+        <div class="row" style="flex-direction: <?php the_sub_field('section_flex-content-movie_order'); ?>">
+            <div class="flex-content__image">
+            <?php 
+            if(get_sub_field('section_flex-content_movie')) :
+                the_sub_field('section_flex-content_movie'); 
             endif; ?>
-        </p>
-    </div>
+            </div>
+            <div class="flex-content__paragraph">
+                <?php
+                if(get_sub_field('section_flex-content-movie_text')) :
+                    the_sub_field('section_flex-content-movie_text');
+                endif; ?>
+            </div>
+        </div>
+
+    <?php
+    elseif( get_row_layout() == 'section_flex-content-text_row' ): ?>
+        <div class="row row-flex-content-text">
+            <?php
+            if(get_sub_field('section_flex-content_text-columns-heading')) :
+                the_sub_field('section_flex-content_text-columns-heading');
+            endif; ?>
+            <p class="flex-content__text" style="column-count: <?php the_sub_field('section_flex-content_text-columns-amount'); ?>">
+                <?php
+                if(get_sub_field('section_flex-content_text-columns')) :
+                    the_sub_field('section_flex-content_text-columns', false, false);
+                endif; ?>
+            </p>
+        </div>
     <?php
     endif;
     endwhile; ?>
