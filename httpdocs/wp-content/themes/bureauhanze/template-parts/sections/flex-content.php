8c54857f3d2b52d2a733d0b415f43024f6da73d0
Bart Nijenhuis <bartnijenhuis@bureauhanze.nl>
2022-05-11 16:05:51 +0200

    added columns to flex content

--- httpdocs/wp-content/themes/bureauhanze/template-parts/sections/flex-content.php
+++ httpdocs/wp-content/themes/bureauhanze/template-parts/sections/flex-content.php
@@ -22,6 +22,21 @@ if( have_rows('section_flex-content') ): ?>
             </div>
         </div>
         <?php
+
+    elseif( get_row_layout() == 'section_flex-content-text_row' ): ?>
+    <div class="row row-flex-content-text">
+        <?php
+        if(get_sub_field('section_flex-content_text-columns-heading')) :
+            the_sub_field('section_flex-content_text-columns-heading');
+        endif; ?>
+        <p class="flex-content__text" style="column-count: <?php the_sub_field('section_flex-content_text-columns-amount'); ?>">
+            <?php
+            if(get_sub_field('section_flex-content_text-columns')) :
+                the_sub_field('section_flex-content_text-columns', false, false);
+            endif; ?>
+        </p>
+    </div>
+    <?php
     endif;
     endwhile; ?>
     </div>
