a670f930d3b26c6c29c94da2efc1a954f522845a
Bart Nijenhuis <bartnijenhuis@bureauhanze.nl>
2022-06-13 15:20:17 +0200

    mobile menu optimalisation

--- httpdocs/wp-content/themes/bureauhanze/template-parts/modals/modal-contact.php
+++ httpdocs/wp-content/themes/bureauhanze/template-parts/modals/modal-contact.php
@@ -1,8 +1,10 @@
 <div class="modal">
     <div class="modal-content">
         <div class="modal-content__header">
-            <h2>Modal contact</h2>
-            <button id="close_modal--contact" class="close close-modal">&#10005;</button>
+            <div class="container">
+                <h2>Modal contact</h2>
+                <button id="close_modal--contact" class="close close-modal">&#10005;</button>
+            </div>
         </div>
         <?php
         get_template_part( 'template-parts/sections/contact' ); 
