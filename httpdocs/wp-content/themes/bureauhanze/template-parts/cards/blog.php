43d706b55f129bdb8f8c3e65d08434423eb7dbdc
Bart Nijenhuis <bartnijenhuis@bureauhanze.nl>
2022-06-15 16:51:04 +0200

    card hover change

--- httpdocs/wp-content/themes/bureauhanze/template-parts/cards/blog.php
+++ httpdocs/wp-content/themes/bureauhanze/template-parts/cards/blog.php
@@ -1,8 +1,8 @@
-<article <?php post_class('blog__card'); ?>>
+<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" <?php post_class('blog__card'); ?>>
     <?php the_post_thumbnail( 'card-blog', array( 'class' => 'card__img lazy' ) ); ?>
     <div class="card__content">
         <h3><?php the_title(); ?></h3>
         <p><?php echo wp_trim_words( get_the_excerpt(), 10 ); ?></p>
-        <a class="btn btn-link-icon" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php get_template_part( 'assets/svg/chevron' ); ?>Bekijk blog</a>
+        <div class="btn btn-link-icon"><?php get_template_part( 'assets/svg/chevron' ); ?>Bekijk blog</div>
     </div>
-</article>
+</a>
