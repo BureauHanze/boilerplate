c46597e486b7bf776fe314eded872505920c29ab
Bart Nijenhuis <bartnijenhuis@bureauhanze.nl>
2022-07-18 10:43:43 +0200

    better layout for flex content

--- httpdocs/wp-content/themes/bureauhanze/template-parts/sections/flex-content.php
+++ httpdocs/wp-content/themes/bureauhanze/template-parts/sections/flex-content.php
@@ -1,7 +1,5 @@
 <?php
 if( have_rows('section_flex-content') ): ?>
-<section class="flex-content">
-    <div class="container">
     <?php
     while ( have_rows('section_flex-content') ) : the_row(); 
     if( get_row_layout() == 'section_flex-content_row' ):
@@ -12,7 +10,5 @@ if( have_rows('section_flex-content') ): ?>
         get_template_part( 'template-parts/flex-content-rows/text-columns' );
     endif;
     endwhile; ?>
-    </div>
-</section>
 <?php
 endif;
